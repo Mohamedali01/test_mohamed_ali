f04887496a58a948b47cf13fe1b7bbe9e122a385
Mohamed Ali <alymohamad91@gmail.com>
2023-05-01 12:35:49 +0300

    qq

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+//test
 void main() {
   runApp(const MyApp());
 }
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page ss'),
     );
   }
 }
